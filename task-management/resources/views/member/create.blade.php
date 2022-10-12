876724c1d127e1df9e24a028836f6bc4b338c15b
quinbez <bezawitt9@gmail.com>
2022-10-13 02:13:41 +0300

    password validation

--- task-management/resources/views/member/create.blade.php
+++ task-management/resources/views/member/create.blade.php
@@ -28,11 +28,79 @@
 
 <div class="row">
     <div class="form-group col-sm-6">
-        {!!Form::label('password','Password: ')!!}
+    {{-- {{-- <div class="form-group col-sm-6"> --}}
+        {{-- {!!Form::label('password','Password: ')!!}
         {!!Form::password('password', ['class'=>'form-control'])!!}
+    </div> --}}
+    <label for="pass1">Password</label>
+    <input type="password" name= "" id="pass1" class="form-control" placeholder="password" onkeyup="return validate()">
+    <label for="pass2">Confirm</label>
+    <input type="password" name ="" id="pass2" class="form-control" placeholder="confirm" oninput =" return confirm()">
+    <div class="errors">
+        <ul style="font-size: 15px" >
+            <li id="upper">atleast one uppercase</li>
+            <li id="lower">atleast one lowercase</li>
+            <li id="special_char">atleast one special symbol</li>
+            <li id="number">atleat one number</li>
+            <li id="length">atleast 8 characters</li>
+        </ul>
     </div>
 </div>
-<div class="row" style="justify-content: right">
+</div>
+<script>
+    function validate(){
+        var pass = document.getElementById('pass1');
+        var upper = document.getElementById('upper');
+        var lower = document.getElementById('lower');
+        var number = document.getElementById('number');
+        var length = document.getElementById('length');
+        var special_char= document.getElementById('special_char');
+
+        if(pass.value.match(/[0-9]/)){
+            number.style.color ="green"
+        }else {
+            number.style.color ="red"
+        }
+        if(pass.value.match(/[A-Z]/)){
+            upper.style.color ="green"
+        }else {
+            upper.style.color ="red"
+        }if(pass.value.match(/[a-z]/)){
+            lower.style.color ="green"
+        }else {
+            lower.style.color ="red"
+        }if(pass.value.match(/[!\@\#\$\%\^\&\*\(\)\_\-\+\=\?\>\<\.\,]/)){
+            special_char.style.color ="green"
+        }else {
+            special_char.style.color ="red"
+        }if(pass.value.length<8){
+            length.style.color ="red"
+        }else{
+            length.style.color = "green"
+        }
+     }
+     function confirm(){
+        var pass1 =document.getElementById('pass1');
+        var pass2 =document.getElementById('pass2');
+        if(pass1.value == pass2.value){
+            document.getElementById('number').style.display = 'none';
+            document.getElementById('length').style.display = 'none';
+            document.getElementById('special_char').style.display = 'none';
+            document.getElementById('lower').style.display = 'none';
+            document.getElementById('upper').style.display = 'none';
+
+        }else{
+            document.getElementById('number').style.display = 'block';
+            document.getElementById('length').style.display = 'block';
+            document.getElementById('special_char').style.display = 'block';
+            document.getElementById('lower').style.display = 'block';
+            document.getElementById('upper').style.display = 'block';
+        }
+     }
+</script>
+
+
+<div class="row" style="justify-content: left">
     <div class="form-group col-sm-3 p-4">
         {!!Form::submit('+ Add', ['class'=>'btn btn-primary'])!!}
         {!!Form::reset('Clear', ['class'=>'btn btn-secondary clearcolor'])!!}
@@ -41,7 +109,7 @@
 </form>
 
 @if(count($errors) > 0)
-    <div class="alert alert-danger">
+    <div class="alert alert-danger" style="width:450px;">
         <ul>
             @foreach($errors->all() as $error)
                 <li>{{$error}}</li>
