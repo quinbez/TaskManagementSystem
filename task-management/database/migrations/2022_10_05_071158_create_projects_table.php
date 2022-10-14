26526757f61433064e27e813f32b80c69169feed
quinbez <bezawitt9@gmail.com>
2022-10-14 09:47:38 +0300

    minor edit

--- task-management/database/migrations/2022_10_05_071158_create_projects_table.php
+++ task-management/database/migrations/2022_10_05_071158_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->string('title');
             $table->integer('category_id');
             $table->string('description');
