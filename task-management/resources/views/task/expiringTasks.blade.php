0ef4903156124146c92c12965929493239776e38
Eden Abera <edujerry.jesus@gmail.com>
2022-10-17 01:13:49 -0900

    task expiring notification

--- task-management/resources/views/task/expiringTasks.blade.php
+++ task-management/resources/views/task/expiringTasks.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h3>New Notifications</h3>
+<h3>Expiring Tasks</h3>
 <table class='table'>
     <thead>
         <tr>
@@ -10,11 +10,14 @@
             <th>Name</th>
             <th>Project</th>
             <th>Assigned To</th>
+            <th>Description</th>
             <th>Start</th>
             <th>End</th>
             <th>Created</th>
             <th>Updated</th>
             <th>Status</th>
+            <th>Update</th>
+            <th>Remove</th>
 
         </tr>
     </thead>
@@ -22,14 +25,19 @@
             @foreach($tasks as $task)
                 <tr>
                     <td>{{$task->id}}</td>
-                    <td>{{$task->name}} @if($task->on_progress == 0 || $task->completed == 0) new @endif</td>
+                    <td>{{$task->name}}</td>
                     <td>{{$task->project?->title}}</td>
                     <td>{{$task->member?->name}}</td>
+                    <td>{{$task->description}}</td>
                     <td>{{$task->start_date}}</td>
                     <td>{{$task->end_date}}</td>
                     <td>{{$task->created_at?->diffForHumans()}}</td>
                     <td>{{$task->updated_at?->diffForHumans()}}</td>
                     <td>{{$task->status}}</td>
+                    <td><a href="{{url("/task/edit/$task->id")}}" style="color:#efef27;">Edit</a></td>
+                    <td><a href="{{url("/task/delete/$task->id")}}" style="color:red;">Delete</a></td>
+
+
                 </tr>
             @endforeach
         @endif
