6c348dc475e523ccf4276b910274d203bc54ca4e
quinbez <bezawitt9@gmail.com>
2022-10-17 13:29:50 +0300

    user with no task

--- task-management/resources/views/user/teamMemberDetails.blade.php
+++ task-management/resources/views/user/teamMemberDetails.blade.php
@@ -2,33 +2,37 @@
 
 @section('content')
 
-<h3>{{$user?->name}}</h3>
-<table class='table'>
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Start-Date</th>
-            <th>Deadline</th>
-            <th>Created</th>
-            <th>Updated</th>
+    <h3>{{ $user?->name }}</h3>
+    <table class='table'>
+        <thead>
+            <tr>
+                <th>Task-Id</th>
+                <th>Task-Name</th>
+                <th>Start-Date</th>
+                <th>Deadline</th>
+                <th>Created</th>
+                <th>Updated</th>
 
 
-        </tr>
-    </thead>
-    <tbody>
-        @if($tasks)
-            @foreach($tasks as $task)
+            </tr>
+        </thead>
+        <tbody>
+            @if ($tasks->count() != 0)
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{{ $task->id }}</td>
+                        <td>{{ $task->name }}</td>
+                        <td>{{ $task->start_date }}</td>
+                        <td>{{ $task->end_date }}</td>
+                        <td>{{ $task->created_at?->diffForHumans() }}</td>
+                        <td>{{ $task->updated_at?->diffForHumans() }}</td>
+                    </tr>
+                @endforeach
+            @else
                 <tr>
-                    <td>{{$task->id}}</td>
-                    <td>{{$task->name}}</td>
-                    <td>{{$task->start_date}}</td>
-                    <td>{{$task->end_date}}</td>
-                    <td>{{$task->created_at?->diffForHumans()}}</td>
-                    <td>{{$task->updated_at?->diffForHumans()}}</td>
+                    <td colspan="6"><h5 style="color:grey;">User has no task</h5></td>
                 </tr>
-            @endforeach
-        @endif
+            @endif
 
-    </tbody>
-@endsection
+        </tbody>
+    @endsection
